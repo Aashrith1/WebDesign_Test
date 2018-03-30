8a7871aa55f5ba0e76105d15e250c8914174fb0c
Aashrith95 <chilkepalli.a@husky.neu.edu>
2018-03-30 02:25:58 -0400

    Test Application

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import{Hero} from './hero';
+
+export const HEROES:Hero[] =[
+    {id:1,name:'Aashrith',age:32},
+    {id:2,name:'Rahul',age:83},
+    {id:3,name:'Sameer',age:11},
+    {id:4,name:'Anand',age:45},
+    {id:5,name:'Dinesh',age:12}   
+];
